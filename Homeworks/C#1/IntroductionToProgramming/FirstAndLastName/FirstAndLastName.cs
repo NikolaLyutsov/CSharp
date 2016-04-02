7c9679c517f5bc1dc43000363a0dd281f8b3f027
NikolaLyutsov <nikola_lyutsov@abv.bg>
2016-04-02 17:50:14 +0300

    Introduction To Programming

---
+++ Homeworks/C#1/IntroductionToProgramming/FirstAndLastName/FirstAndLastName.cs
@@ -0,0 +1,12 @@
+﻿//Create console application that prints your first and last name, each at a separate line.
+
+using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Nikola");
+        Console.WriteLine("Lyutsov");
+        }
+    }
