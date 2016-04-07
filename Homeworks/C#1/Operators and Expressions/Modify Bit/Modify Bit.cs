9c2ba057852264f238124739da4f89683f98e296
NikolaLyutsov <nikola_lyutsov@abv.bg>
2016-04-07 16:46:36 +0300

    Operators and Expressions

---
+++ Homeworks/C#1/Operators and Expressions/Modify Bit/Modify Bit.cs
@@ -0,0 +1,29 @@
+﻿//We are given an integer number N(read from the console), a bit value v(read from the console as well)
+//(v = 0 or 1) and a position P(read from the console). Write a sequence of operators(a few lines of C# 
+//code) that modifies N to hold the value v at the position P from the binary representation of N while 
+//preserving all other bits in N. Print the resulting number on the console.
+
+using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        ulong n = ulong.Parse(Console.ReadLine());
+        int p = int.Parse(Console.ReadLine());
+        byte v = byte.Parse(Console.ReadLine());
+
+        if(v == 0)
+        {
+            ulong mask = (ulong)~(1 << p);
+            ulong result = n & mask;
+            Console.WriteLine(result);
+        }
+        else
+        {
+            ulong mask = (ulong)1 << p;
+            ulong result = n | mask;
+            Console.WriteLine(result);
+        }
+    }
+    }
